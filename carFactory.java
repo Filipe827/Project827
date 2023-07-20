f813fb4fa878c2c3b22ca2bc743d2c724e7ffb0f
Filipe827 <110000036+Filipe827@users.noreply.github.com>
2023-07-20 20:27:10 +0100

    Update carFactory.java
    Added tire parameter

--- carFactory.java
+++ carFactory.java
@@ -6,23 +6,22 @@ public class carFactory {
 
     }
 
-    public Car createCalliope(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage){
-        return new Car(new spindlerBattery(lastServiceDate), new capuletEngine(lastServiceMileage, currentMileage));
+    public Car createCalliope(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage, double[] tireWear){
+        return new Car(new spindlerBattery(lastServiceDate), new capuletEngine(lastServiceMileage, currentMileage), new carriganTire(tireWear));
     }
 
-    public Car createGlissade(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage){
-        return new Car(new spindlerBattery(lastServiceDate), new willoughbyEngine(lastServiceMileage, currentMileage));
+    public Car createGlissade(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage, double[] tireWear){
+        return new Car(new spindlerBattery(lastServiceDate), new willoughbyEngine(lastServiceMileage, currentMileage), new octoprimeTire(tireWear));
     }
 
-    public Car createPalindrome(LocalDate lastServiceDate, Boolean warningLightOn){
-        return new Car(new spindlerBattery(lastServiceDate), new sternmanEngine(warningLightOn));
+    public Car createPalindrome(LocalDate lastServiceDate, Boolean warningLightOn, double[] tireWear){
+        return new Car(new spindlerBattery(lastServiceDate), new sternmanEngine(warningLightOn), new carriganTire(tireWear));
     }
 
-    public Car createRorschach(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage){
-        return new Car(new nubbinBattery(lastServiceDate), new willoughbyEngine(lastServiceMileage,currentMileage));
+    public Car createRorschach(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage, double[] tireWear){
+        return new Car(new nubbinBattery(lastServiceDate), new willoughbyEngine(lastServiceMileage,currentMileage), new octoprimeTire(tireWear));
     }
 
-    public Car createThovex(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage){
-        return new Car(new nubbinBattery(lastServiceDate), new capuletEngine(lastServiceMileage, currentMileage));
+    public Car createThovex(LocalDate lastServiceDate, int currentMileage, int lastServiceMileage, double[] tireWear){
+        return new Car(new nubbinBattery(lastServiceDate), new capuletEngine(lastServiceMileage, currentMileage), new carriganTire(tireWear));
     }
-}
